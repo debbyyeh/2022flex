d03c52afb29f3b345f051b266eca78e91ae8e97b
debby <app2984@gmail.com>
2022-05-24 00:03:25 +0800

    update js

--- src/all.js
+++ src/all.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+  // burger menu
   $('.menu_btn').click(function (e) {
     e.preventDefault(), $('body').toggleClass('menuList-height')
+  })
+
+  // 發票頁面
+  $('.receiptButton a').click(function (e) {
+    e.preventDefault()
+    $(this).addClass('active').siblings().removeClass('active')
+  })
+  $('.E-receiptButton').click(function (e) {
+    $('.E-receiptContent').show()
+    $('.paper-receiptContent').hide()
+  })
+  $('.paper-receiptButton').click(function (e) {
+    $('.paper-receiptContent').show()
+    $('.E-receiptContent').hide()
   })
 })
